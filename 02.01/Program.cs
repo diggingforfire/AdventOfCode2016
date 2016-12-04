42b2c1a2c4dc0477ae5ce6cde1c79c550c62d4bc
Robbie Luijben <robbie.luijben@snappet.org>
2016-12-04 12:11:44 +0100

    Day 2.1

---
+++ 02.01/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace _02._01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int pos = 5, min = 1, max = 9, gridSize = 3;
+      
+            var code = File.ReadAllLines("input.txt").Aggregate("", (a, b) =>
+            {
+                b.Select(c => c).ToList().ForEach(c =>
+                {
+                    pos += (c == 'U' && pos - gridSize >= min) ? -gridSize :
+                            (c == 'D' && pos + gridSize <= max) ? gridSize :
+                            (c == 'L' && pos % gridSize != min) ? -min :
+                            (c == 'R' && pos % gridSize >= min) ? min : 0;
+                });
+
+                return a + pos.ToString();
+            });
+
+            Console.WriteLine(code);
+            Console.ReadKey();
+        }
+    }
+}
