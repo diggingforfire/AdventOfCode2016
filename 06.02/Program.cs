93f93d8246524878eb028115f13587028f5dc67a
Robbie Luijben <robbie.luijben@snappet.org>
2016-12-06 09:58:36 +0100

    Day 6

---
+++ 06.02/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace _06._02
+{
+    class Program
+    {
+        static void Main()
+        {
+            var result =
+                File.ReadAllLines("input.txt")
+                .SelectMany(line => line.Select((@char, index) => new { @char, index }))
+                .GroupBy(g => g.index, (key, group) => group.ToList().Select(c => c.@char).ToList())
+                .Select(g => g.GroupBy(p => p, (key, group) =>
+                new
+                {
+                    key,
+                    count = group.Count()
+                }).OrderBy(o => o.count).First())
+               .Select(p => p.key.ToString())
+               .Aggregate((a, b) => a.ToString() + b.ToString());
+
+                    Console.WriteLine(result);
+                    Console.ReadKey();
+        }
+    }
+}
